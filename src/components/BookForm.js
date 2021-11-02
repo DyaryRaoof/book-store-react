9c822742b619f9810fe5e2eebc6e28604d6f7536
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-11-02 20:43:23 +0300

    Add form validation for BookForm componenet

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -8,6 +8,7 @@ const BookForm = () => {
   const books = useSelector((state) => state.bookReducer);
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [error, setError] = useState('');
 
   const addBookToStore = (e) => {
     e.preventDefault();
@@ -16,9 +17,15 @@ const BookForm = () => {
       title,
       author,
     };
-    dispatch(addBook(book));
-    setTitle('');
-    setAuthor('');
+
+    if (title && author) {
+      dispatch(addBook(book));
+      setTitle('');
+      setAuthor('');
+      setError(null);
+    } else {
+      setError('Title and author should not be empty !');
+    }
   };
 
   return (
@@ -31,14 +38,18 @@ const BookForm = () => {
             className="book-title"
             onChange={(e) => { setTitle(e.target.value); }}
             value={title}
+            minLength="1"
           />
           <input
             placeholder="Book author"
             onChange={(e) => { setAuthor(e.target.value); }}
             value={author}
+            minLength="1"
           />
           <button type="submit" onClick={(e) => { addBookToStore(e); }}>Add Book</button>
         </form>
+        {error ? <span className="error-span">{error}</span> : null}
+
       </div>
 
     </div>
