028266e0d195cb7ddbf98e86f0d4ff42df27bc13
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-11-02 19:52:59 +0300

    Add addBook and removeBookfunctionality through redux

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,17 +1,48 @@
 import './BookForm.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { addBook } from '../redux/books/books';
 
-const BookForm = () => (
-  <div className="book-form-wrapper">
-    <div>
-      <div>Add New Book</div>
-      <form className="book-form">
-        <input placeholder="Book title" className="book-title" />
-        <input placeholder="Book author" />
-        <button type="submit">Add Book</button>
-      </form>
+const BookForm = () => {
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.bookReducer);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const addBookToStore = (e) => {
+    e.preventDefault();
+    const book = {
+      id: books.length,
+      title,
+      author,
+    };
+    dispatch(addBook(book));
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <div className="book-form-wrapper">
+      <div>
+        <div>Add New Book</div>
+        <form className="book-form">
+          <input
+            placeholder="Book title"
+            className="book-title"
+            onChange={(e) => { setTitle(e.target.value); }}
+            value={title}
+          />
+          <input
+            placeholder="Book author"
+            onChange={(e) => { setAuthor(e.target.value); }}
+            value={author}
+          />
+          <button type="submit" onClick={(e) => { addBookToStore(e); }}>Add Book</button>
+        </form>
+      </div>
+
     </div>
-
-  </div>
-);
+  );
+};
 
 export default BookForm;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import store from './redux/configureStore';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
